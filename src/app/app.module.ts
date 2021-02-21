cb08579ee8fa2891ed0ab6e915a07ac858ea4832
ThippayawadeeS <Thippayawadees@gmail.com>
2021-02-21 13:11:45 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CountingComponent } from './counting/counting.component';
+import { Lab4Component } from './lab4/lab4.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent, CountingComponent
+    AppComponent, CountingComponent,Lab4Component
   ],
   imports: [
     BrowserModule //,FormsModule
